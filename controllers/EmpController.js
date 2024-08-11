e0043c0f9a5c54c0328dc63e26c13606ec7241b6
DronesSaifAkash <dronespictur222@gmail.com>
2024-08-11 11:45:14 +0530

    nw

--- controllers/EmpController.js
+++ controllers/EmpController.js
@@ -11,6 +11,40 @@ class EmpController {
         }
     };
 
+
+    static showbyid = async (req, res) => {
+        try{ 
+            let eid = req.params.eid;
+
+            let data = await emp.findById(eid);
+
+            res.status(200).json(data)
+            
+        }catch(error){
+            console.log(error);
+        }
+    // http://localhost:4000/emp/showbyid/66b844b286296489435919bd
+    }
+
+
+    static showbyloc = async (req, res) => {
+        try{ 
+            let loc = req.params.loc;
+
+            let data = await emp.find({loc: loc});
+
+            if(data.length === 0 ){
+                return res.status(404).json({'mesg':'Boo! no emp.'})
+            }
+            res.status(200).json(data)
+            
+        }catch(error){
+            console.log(error);
+        }
+    // http://localhost:4000/emp/showbyloc/kol
+    }
+
+
 }
 
 module.exports = EmpController;
--- routes/empRoute.js
+++ routes/empRoute.js
@@ -6,6 +6,12 @@ app.get('/', (req,res) => {
 })
 
 const EmpController = require('../controllers/EmpController');
+
 app.get('/showAll',EmpController.showAll);
+
+app.get('/showbyid/:eid', EmpController.showbyid);
+
+app.get('/showbyloc/:loc', EmpController.showbyloc);
+
 
 module.exports = app; 
